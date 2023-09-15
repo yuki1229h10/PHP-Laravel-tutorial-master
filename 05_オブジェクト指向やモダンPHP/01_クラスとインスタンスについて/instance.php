5e380d946e3be834ec11e6c3dd1b82eb65688f8d
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-09-15 23:55:09 +0900

    yet

--- 05_オブジェクト指向やモダンPHP/01_クラスとインスタンスについて/instance.php
+++ 05_オブジェクト指向やモダンPHP/01_クラスとインスタンスについて/instance.php
@@ -3,6 +3,19 @@
 class Product
 {
 
-  // アクセス修飾子, private(外からアクセスできない), protected(自分・継承したクラス),
+  // アクセス修飾子, private(外からアクセスできない), protected(自分・継承したクラス), public(公開)
 
+  // 変数
+  private $product = [];
+
+  // 関数
+  function __construct($product)
+  {
+    $this->product = $product;
+  }
+
+  public function getProduct()
+  {
+    echo $this->product;
+  }
 }
