e099d257bff6432bffadf09376dcaa4252338645
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-09-21 17:01:47 +0900

    set model for view

--- 06_Laravel9 introduction/task_test/app/Http/Controllers/TestController.php
+++ 06_Laravel9 introduction/task_test/app/Http/Controllers/TestController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;
 
 class TestController extends Controller
 {
     public function index()
     {
-        return view('tests.test');
+        $values = Test::all();
+
+        // dd($values);
+
+        return view('tests.test', compact('values'));
     }
 }
