eff9575172a36bcc313ab27bfc5de4775e505841
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-08 22:55:29 +0900

    use migrate

--- 07_簡易Webアプリ/task_test/database/migrations/2023_10_06_183303_add_title_to_contact_forms_table.php
+++ 07_簡易Webアプリ/task_test/database/migrations/2023_10_06_183303_add_title_to_contact_forms_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('contact_forms', function (Blueprint $table) {
-            //
+            $table->dropColumn('title');
         });
     }
 };
--- 07_簡易Webアプリ/test.php
+++
@@ -1,5 +0,0 @@
-<?php echo 'hi';
-echo 'test';
-
-echo '草が生えない';
-echo 'again';
