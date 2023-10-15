2f62e623bcabdf10e85276d65e23783466159f90
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-15 15:39:10 +0900

    create show view

--- 07_簡易Webアプリ/task_test/app/Http/Controllers/ContactFormController.php
+++ 07_簡易Webアプリ/task_test/app/Http/Controllers/ContactFormController.php
@@ -60,7 +60,33 @@ class ContactFormController extends Controller
     public function show($id)
     {
         $contact = ContactForm::find($id);
-        return view('contacts.show', compact('contact'));
+
+        if ($contact->gender === 0) {
+            $gender = '男性';
+        } else {
+            $gender = '女性';
+        }
+
+        if ($contact->age === 1) {
+            $age = '~19歳';
+        }
+        if ($contact->age === 2) {
+            $age = '20~29歳';
+        }
+        if ($contact->age === 3) {
+            $age = '30~39歳';
+        }
+        if ($contact->age === 4) {
+            $age = '40~49歳';
+        }
+        if ($contact->age === 5) {
+            $age = '50~59歳';
+        }
+        if ($contact->age === 6) {
+            $age = '60歳~';
+        }
+
+        return view('contacts.show', compact('contact', 'gender', 'age'));
     }
 
     /**
