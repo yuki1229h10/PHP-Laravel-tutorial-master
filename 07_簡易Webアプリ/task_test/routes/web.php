28c771c586bf3047631bdb4febd20854202639d8
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-10-18 14:42:06 +0900

    create shops

--- 07_簡易Webアプリ/task_test/routes/web.php
+++ 07_簡易Webアプリ/task_test/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\ContactFormController;
+use App\Http\Controllers\ShopController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,7 @@ use App\Http\Controllers\ContactFormController;
 */
 
 Route::get('tests/test', [TestController::class, 'index']);
+Route::get('shops', [ShopController::class, 'index']);
 
 // Route::resource('contacts', ContactFormController::class);
 
